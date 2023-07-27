3f2b479fc5fbb9a0e88e0aa3f8b8f933bf6a9517
CoderSerio <CoderSerio@2779066456@qq.com>
2023-07-28 03:58:36 +0800

    feat: 初步实现useState

--- demos/demo1/src/index.js
+++ demos/demo1/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const jsx = (
-	<div>
-		<span>Hello World</span>
-	</div>
-);
+function App() {
+	return (
+		<div>
+			<span>Hello World</span>
+		</div>
+	);
+}
 
 const rootDOM = document.querySelector('#root');
-
 const root = ReactDOM.createRoot(rootDOM);
-root.render(jsx);
+root.render(<App></App>);
 
 console.log(React);
-console.log(jsx);
 console.log(ReactDOM);
 console.log(root);
